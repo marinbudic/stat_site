38669450a0ad5f87dbf16dab0a507428984b6b3f
marinbudic <mabudic@gmail.com>
2025-04-10 17:41:32 -0500

    change from moving to public folder to docs folder

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from extract import extract_title
 
 import os
 import shutil
+import sys
 
 def move_files(stat, pub):
     if os.path.exists(pub):
@@ -24,7 +25,7 @@ def move_files(stat, pub):
             print(f"Copying {path} to {pub_path}")
             move_files(path, pub_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md = ""
     template = ""
@@ -42,12 +43,15 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", heading)
     new_html = template.replace("{{ Content }}", html)
 
+    new_html = new_html.replace('href="/', f'href="{basepath}')
+    new_html = new_html.replace('src="/', f'src="{basepath}')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w",  encoding="utf-8") as f:
         f.write(new_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     children = os.listdir(dir_path_content)
 
     for child in children:
@@ -57,22 +61,27 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             filename_without_ext = os.path.splitext(child)[0]
             html_filename = filename_without_ext + ".html"
             dest_file_path = os.path.join(dest_dir_path, html_filename)
-            generate_page(path, template_path, dest_file_path)
+            generate_page(path, template_path, dest_file_path, basepath)
         else:
             pub_path = os.path.join(dest_dir_path, child)
             os.mkdir(pub_path)
-            generate_pages_recursive(path, template_path, pub_path)
+            generate_pages_recursive(path, template_path, pub_path, basepath)
 
 def main():
     static = "/home/marinbudic/bootprojs/stat_site/static/"
-    public = "/home/marinbudic/bootprojs/stat_site/public/"
+    docs = "/home/marinbudic/bootprojs/stat_site/docs/"
 
-    move_files(static, public)
+    move_files(static, docs)
 
     content = "/home/marinbudic/bootprojs/stat_site/content/"
     template = "/home/marinbudic/bootprojs/stat_site/template.html"
-    destination = "/home/marinbudic/bootprojs/stat_site/public/"
+    destination = "/home/marinbudic/bootprojs/stat_site/docs/"
+        
+    basepath = "/"
 
-    generate_pages_recursive(content, template, destination)
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    generate_pages_recursive(content, template, destination, basepath)
 
 main()
